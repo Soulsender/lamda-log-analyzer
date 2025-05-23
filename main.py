20fcf8f93420b451e95ccb082aa9ee4fe9444db2
Soulsender <soul@soulsender.me>
2025-05-23 10:23:41 -0700

    update

--- main.py
+++ main.py
@@ -3,30 +3,32 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 print('Loading function')
 
-# config
-bucket = 'splunk-logs-cisa-group2'
+def lambda_handler(context, event):
 
-client = boto3.client('s3')
-response = client.list_objects(Bucket=bucket)['Contents']
-items = 0
-list_of_items = ""
-list_of_storage_class = ""
-list_of_last_modified = ""
+    # config
+    bucket = 'splunk-logs-cisa-group2'
 
-for item in response:
-    items += 1
-    list_of_items = list_of_items + "\n" + item['Key']
-    list_of_storage_class = list_of_storage_class + "\n" + item['StorageClass']
-    list_of_last_modified = list_of_last_modified + "\n" + str(item['LastModified'])
+    client = boto3.client('s3')
+    response = client.list_objects(Bucket=bucket)['Contents']
+    items = 0
+    list_of_items = ""
+    list_of_storage_class = ""
+    list_of_last_modified = ""
 
-print(response)
+    for item in response:
+        items += 1
+        list_of_items = list_of_items + "\n" + item['Key']
+        list_of_storage_class = list_of_storage_class + "\n" + item['StorageClass']
+        list_of_last_modified = list_of_last_modified + "\n" + str(item['LastModified'])
 
-webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1375509400994971758/Iam9qNCW_Dt68KIKQlx7jDV4onQGfbErNcsqg8XKEmxQ_CiGYEw0PuuIGjajSpkjT9_w')
-embed = DiscordEmbed(title='Amazon S3', description=f"AWS S3 has been updated with **{items}** item(s):", color='FF9900')
-embed.add_embed_field(name="Items in S3:", value=f"{list_of_items}")
-embed.add_embed_field(name="Storage Class:", value=f"{list_of_storage_class}")
-embed.add_embed_field(name="Last Modified:", value=f"{list_of_last_modified}")
+    print(response)
 
-webhook.add_embed(embed)
-webhook.execute()
+    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/1375509400994971758/Iam9qNCW_Dt68KIKQlx7jDV4onQGfbErNcsqg8XKEmxQ_CiGYEw0PuuIGjajSpkjT9_w')
+    embed = DiscordEmbed(title='Amazon S3', description=f"AWS S3 has been updated with **{items}** item(s):", color='FF9900')
+    embed.add_embed_field(name="Items in S3:", value=f"{list_of_items}")
+    embed.add_embed_field(name="Storage Class:", value=f"{list_of_storage_class}")
+    embed.add_embed_field(name="Last Modified:", value=f"{list_of_last_modified}")
+
+    webhook.add_embed(embed)
+    webhook.execute()
 
